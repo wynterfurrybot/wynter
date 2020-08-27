5b5888b44be463fa55b837b6bded6df9aeddcee9
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-08-27 11:43:30 +0100

    Added ping command, as required by some botlists

---
+++ src/commands/ping.ts
@@ -0,0 +1,24 @@
+import { Message, MessageEmbed } from 'discord.js';
+
+import Command from '../lib/structures/Command';
+
+export default class extends Command {
+	public constructor() {
+		super({
+			name: 'ping',
+			cooldown: 5,
+			usage: '',
+		});
+	}
+
+	public async run(msg: Message): Promise<Message> {
+		return msg.channel.send(
+			new MessageEmbed()
+				.setColor(0x00ff00)
+				.setTitle('Pong')
+				.setDescription(
+					`Response got in ${Math.round(this.client.ws.ping)} ms`,
+				),
+		);
+	}
+}
