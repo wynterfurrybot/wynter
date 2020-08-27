664f4d0c8ac02f348bff1832dd6a01cae3228110
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-08-27 22:41:50 +0100

    Snuggle now uses Wynter API

--- src/commands/snuggle.ts
+++ src/commands/snuggle.ts
@@ -2,6 +2,8 @@ import { MessageEmbed, Message } from 'discord.js';
 
 import Command from '../lib/structures/Command';
 
+import axios from 'axios';
+
 export default class extends Command {
 	public constructor() {
 		super({
@@ -13,26 +15,63 @@ export default class extends Command {
 	}
 
 	public async run(msg: Message): Promise<Message> {
-		if (msg.mentions.users.first()!.id === msg.author!.id) {
-			return msg.channel.send(
-				new MessageEmbed()
-					.setColor(0x00ff00)
-					.setTitle('Hug!')
-					.setDescription(`${msg.author} has snuggled themselves, what a loner!`)
-					.setThumbnail(
-						'https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg',
-					),
-			);
-		} else {
-			return msg.channel.send(
-				new MessageEmbed()
-					.setColor(0x00ff00)
-					.setTitle('Hug!')
-					.setDescription(`${msg.author} has snuggled ${msg.mentions.users.first()} super tightly!`)
-					.setThumbnail(
-						'https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg',
-					),
-			);
-		}
+		axios
+			.get('https://api.furrycentr.al/sfw/hug')
+			.then(function (response) {
+				console.log(response.data.result.imgUrl);
+				if (msg.mentions.users.first()!.id === msg.author!.id) {
+					return msg.channel.send(
+						`**${msg.author.username}** has snuggled themselves, what a loner!`,
+						new MessageEmbed()
+							.setColor(0x00ff00)
+							.setDescription(`[Direct Image](${response.data.result.imgUrl})`)
+							.setImage(response.data.result.imgUrl),
+					);
+				} else {
+					return msg.channel.send(
+						`**${msg.author.username}** has snuggled **${
+							msg.mentions.users.first()!.username
+						}** tightly!`,
+						new MessageEmbed()
+							.setColor(0x00ff00)
+							.setDescription(`[Direct Image](${response.data.result.imgUrl})`)
+							.setImage(response.data.result.imgUrl),
+					);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+				if (msg.mentions.users.first()!.id === msg.author!.id) {
+					return msg.channel.send(
+						`**${msg.author.username}** has snuggled themselves, what a loner!`,
+						new MessageEmbed()
+							.setColor(0x00ff00)
+							.setDescription(
+								'[Direct Image](https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg)',
+							)
+							.setFooter('Wynter API is down | Showing static image.')
+							.setImage(
+								'https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg',
+							),
+					);
+				} else {
+					return msg.channel.send(
+						`**${msg.author.username}** has snuggled **${
+							msg.mentions.users.first()!.username
+						}** tightly!`,
+						new MessageEmbed()
+							.setColor(0x00ff00)
+							.setDescription(
+								'[Direct Image](https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg)',
+							)
+							.setFooter('Wynter API is down | Showing static image.')
+							.setImage(
+								'https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg',
+							),
+					);
+				}
+			});
+
+		return msg;
 	}
 }
