e44a7ec0420b18c54be95c457e88d9f8ce0f0713
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-08-31 18:12:38 +0100

    Updated kiss to use Wynter API

--- src/commands/kiss.ts
+++ src/commands/kiss.ts
@@ -1,6 +1,7 @@
 import { MessageEmbed, Message } from 'discord.js';
 
 import Command from '../lib/structures/Command';
+import axios from 'axios';
 
 export default class extends Command {
 	public constructor() {
@@ -12,14 +13,38 @@ export default class extends Command {
 	}
 
 	public async run(msg: Message): Promise<Message> {
-		return msg.channel.send(
-			new MessageEmbed()
-				.setColor(0x00ff00)
-				.setTitle('Kiss!')
-				.setDescription(`${msg.author} has given ${msg.mentions.users.first()} a kiss on the cheek`)
-				.setThumbnail(
-					'https://external-preview.redd.it/Au9jfafmj8FSCgkzvu1odMbw7tLRaye5hWXv5z-Ezus.png?auto=webp&s=bdd727bc7ae09d71521e6d8cec18ef1f69d0a207',
-				),
-		);
+		axios
+			.get('https://api.furrycentr.al/sfw/kiss')
+			.then(function (response) {
+				console.log(response.data.result.imgUrl);
+				return msg.channel.send(
+					`**${msg.member!.displayName}** has kissed **${
+						msg.mentions.members!.first()!.displayName
+					}** on their cheek!`,
+					new MessageEmbed()
+						.setColor(0x00ff00)
+						.setDescription(`[Direct Image](${response.data.result.imgUrl})`)
+						.setImage(response.data.result.imgUrl),
+				);
+			})
+			.catch(function (error) {
+				console.log(error);
+				return msg.channel.send(
+					`**${msg.member!.displayName}** has kissed **${
+						msg.mentions.members!.first()!.displayName
+					}** on their cheek!`,
+					new MessageEmbed()
+						.setColor(0x00ff00)
+						.setDescription(
+							'[Direct Image](https://external-preview.redd.it/Au9jfafmj8FSCgkzvu1odMbw7tLRaye5hWXv5z-Ezus.png?auto=webp&s=bdd727bc7ae09d71521e6d8cec18ef1f69d0a207)',
+						)
+						.setFooter('Wynter API is down | Showing static image.')
+						.setImage(
+							'https://external-preview.redd.it/Au9jfafmj8FSCgkzvu1odMbw7tLRaye5hWXv5z-Ezus.png?auto=webp&s=bdd727bc7ae09d71521e6d8cec18ef1f69d0a207',
+						),
+				);
+			});
+
+		return msg;
 	}
 }
