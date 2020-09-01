8a2fd4500d191a039b5d0c3a50635ca7be463361
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-09-01 20:54:32 +0100

    Fixed being able to slap self

--- src/commands/slap.ts
+++ src/commands/slap.ts
@@ -1,25 +1,36 @@
-import { MessageEmbed, Message } from 'discord.js';
+import { MessageEmbed, Message, TextChannel } from 'discord.js';
 
 import Command from '../lib/structures/Command';
 
 export default class extends Command {
 	public constructor() {
 		super({
-			name: 'slap',
+			name: 'yiff',
+			aliases: ['fuck'],
 			cooldown: 5,
 			usage: '<member>',
 		});
 	}
 
 	public async run(msg: Message): Promise<Message> {
+		if ((msg.channel as TextChannel).nsfw === false) {
+			return msg.channel.send(
+				new MessageEmbed()
+					.setColor(0x00ff00)
+					.setTitle('Not A NSFW Channel')
+					.setDescription('The channel you just ran the command in is not NSFW!')
+					.setThumbnail('https://freeiconshop.com/wp-content/uploads/edd/cross-flat.png'),
+			);
+		}
+		if (msg.author.id === msg.mentions.users.first()!.id) return msg;
 		return msg.channel.send(
 			new MessageEmbed()
 				.setColor(0x00ff00)
-				.setTitle('Trout Slap!')
-				.setDescription(
-					`${msg.author} slaps ${msg.mentions.users.first()} around a bit with a wet trout!`,
-				)
-				.setThumbnail('https://i.pinimg.com/600x315/c7/eb/77/c7eb77dfaa7628a6c3438cd0139bcb78.jpg'),
+				.setTitle('Yiff!')
+				.setDescription(`${msg.author} has yiffed ${msg.mentions.users.first()} hard!`)
+				.setThumbnail(
+					'https://ci.phncdn.com/videos/201812/04/195030881/original/(m=eaAaGwObaaaa)(mh=0KDlKJW31QShbuqU)14.jpg',
+				),
 		);
 	}
 }
