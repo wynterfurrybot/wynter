7bf467a6f0e2a53d1e6c3a98f3cf35d82dbff534
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-10-24 14:39:42 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -611,8 +611,8 @@ client.on('message', async (msg) => {
 
 	let staff = false;
 	
-	if(msg.author!.bot) {
-		if (msg.channel.id === '763159605479079956'){
+	if(msg.author!.bot === true) {
+		if (msg.channel.id === '769555432456454164'){
 			msg.channel.send('SMH. Bot commands go in <#763159637527756820> or <#763159688942190623>! \n\nIf this message appeared in error, please ignore it');
 		}
 	
