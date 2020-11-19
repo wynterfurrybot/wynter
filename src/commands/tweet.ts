36829bed243edfc11d07af2144f3d433c3562fff
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-11-19 01:39:48 +0000

    allow cf to tweet from a channel

--- src/commands/tweet.ts
+++ src/commands/tweet.ts
@@ -33,17 +33,35 @@ export default class extends Command {
 			);
 		}
 
-		const user = await FindUser(msg.author.id);
-		if (!user) {
-			return msg.channel.send(
-				new MessageEmbed()
-					.setColor(0x00ff00)
-					.setTitle('No Twitter Account Linked!')
-					.setDescription(
-						'I cannot tweet when you have no account linked! \nPlease link an account by going to https://furrycentr.al/twitter',
-					)
-					.setThumbnail('https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-articleLarge-v4.jpg?quality=75&auto=webp&disable=upscale'),
-			);
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		let user: any;
+
+		if(msg.channel.id === '778794459562508298'){
+			user = await FindUser('1');
+			if (!user) {
+				return msg.channel.send(
+					new MessageEmbed()
+						.setColor(0x00ff00)
+						.setTitle('No Twitter Account Linked!')
+						.setDescription(
+							'I cannot tweet when you have no account linked! \nPlease link an account by going to https://furrycentr.al/twitter',
+						)
+						.setThumbnail('https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-articleLarge-v4.jpg?quality=75&auto=webp&disable=upscale'),
+				);
+			}
+		} else{
+			user = await FindUser(msg.author.id);
+			if (!user) {
+				return msg.channel.send(
+					new MessageEmbed()
+						.setColor(0x00ff00)
+						.setTitle('No Twitter Account Linked!')
+						.setDescription(
+							'I cannot tweet when you have no account linked! \nPlease link an account by going to https://furrycentr.al/twitter',
+						)
+						.setThumbnail('https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-articleLarge-v4.jpg?quality=75&auto=webp&disable=upscale'),
+				);
+			}
 		}
 
 		const client = new Twitter({
